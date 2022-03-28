6312e8971f9e8841a92f93a5c66c61abf1c2db77
Keiva <keiva@silverwire.tech>
2022-03-27 20:31:38 -0700

    Complete 'gui.py'. by Keiva

---
+++ README.txt
@@ -0,0 +1,1 @@
+A quick Python-based raffle game utilizing Pygame.
--- gui.py
+++ gui.py
@@ -5,5 +5,19 @@ from turtle import width
 pygame.init()
 
 size = width, height = 500, 500
+speed = [2,2]
 black = 0,0,0
 
+screen = pygame.display.set_mode(size)
+
+wheel = pygame.image.load("raffle_wheel.svg")
+wheelspot = wheel.get_rect()
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+    wheelspot = pygame.transform.rotate(wheelspot,5)
+
+    screen.fill(black)
+    screen.blit(wheel, wheelspot)
+    pygame.display.flip()
